58b88413e568ff4273c77b02788a9a13d4f12f21
Juliana Nielson <94935811+SummerSkies@users.noreply.github.com>
2024-05-16 15:08:07 -0600

    Missed one file in the last commit.

--- Scripting Essentials/C# Basics/Basics/001 Conventions and Syntax/HelloWorld.cs
+++ Scripting Essentials/C# Basics/Basics/001 Conventions and Syntax/HelloWorld.cs
@@ -2,11 +2,11 @@
 //Make sure your work is different from the instructors examples or your peers
 using System;
 
-public class HelloWorld
+public class CorrectTheErrors
 {
     public static void Main(string[] args)
     {
-        //Change "HW" to "Hello World"
-        Console.WriteLine("Hello World");
-    }    
+        Console.WriteLine("HW");
+    }
+      
 }
