d5b968c8ba4b27149572a90cf4bb1fc2dfc3befe
Juliana Nielson <nielsonjuliana@gmail.com>
2024-06-15 18:30:58 -0600

    Upgraded Basic Character Controller

---
+++ The-Ghost-Game-Reborn/Assets/ThirdPersonController.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    [Header("Movement Speeds")]
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private float sprintMultiplier = 2.0f;
+    [SerializeField] private float crouchMultiplier = 0.5f;
+
+    [Header("Look Sensitivity")]
+    [SerializeField] private float mouseSensitivity = 2.0f;
+    [SerializeField] private float upDownRange = 80.0f;
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float gravity = 9.81f;
+
+    private CharacterController characterController;
+    private Camera mainCamera;
+    private PlayerInputManager playerInputManager;
+    private Vector3 currentMovement;
+    private float verticalRotation;
+
+    private void Awake()
+    {
+        //Access the character controller, camera, and input manager instance
+        characterController = GetComponent<CharacterController>();
+        mainCamera = Camera.main;
+        playerInputManager = PlayerInputManager.Instance;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+
+    void HandleMovement()
+    {
+        float speed;
+        
+        //Sets player speed
+        if (playerInputManager.CrouchTriggered)
+        {
+            speed = moveSpeed * crouchMultiplier;
+        }
+        else if (playerInputManager.SprintTriggered)
+        {
+            speed = moveSpeed * sprintMultiplier;
+        }
+        else
+        {
+            speed = moveSpeed;
+        }
+
+        //Transform player direction to world space, normalize to prevent speed from getting too high
+        Vector3 inputDirection = new Vector3(playerInputManager.MoveInput.x, 0.0f, playerInputManager.MoveInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        worldDirection.Normalize();
+
+        //Apply speed to direction
+        currentMovement.x = worldDirection.x * speed;
+        currentMovement.z = worldDirection.z * speed;
+
+        HandleJumping();
+
+        //Move character
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    void HandleJumping()
+    {
+        if (characterController.isGrounded)
+        {
+            currentMovement.y = -0.5f;
+
+            if (playerInputManager.JumpTriggered)
+            {
+                currentMovement.y = jumpForce;
+            }
+        }
+        else
+        {
+            currentMovement.y -= gravity * Time.deltaTime;
+        }
+    }
+
+    void HandleRotation()
+    {
+        //Set horizontal look rotation
+        float mouseXRotation = playerInputManager.LookInput.x * mouseSensitivity;
+        transform.Rotate(0, mouseXRotation, 0);
+
+        //Set vertical look rotation within upDownRange
+        verticalRotation -= playerInputManager.LookInput.y * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+}
