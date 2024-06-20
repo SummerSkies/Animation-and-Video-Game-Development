9855a7962c99d6de7b8925bf3aa1bbe9c2a1efad
Juliana Nielson <nielsonjuliana@gmail.com>
2024-06-19 18:19:54 -0600

    Step Into the Driver's Seat

--- Scripting Essentials/Driving Simulator/DrivingSimulator/Assets/Scripts/TruckController.cs
+++ Scripting Essentials/Driving Simulator/DrivingSimulator/Assets/Scripts/TruckController.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class TruckController : MonoBehaviour
 {
+    [Header("Truck Movement")]
+
     [SerializeField] private float truckMovementSpeed = 20.0f;
+    [SerializeField] private float truckTurnSpeed = 60.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
 
     // Update is called once per frame
     private void Update()
     {
-        //Move the truck forward
-        transform.Translate(Vector3.forward * Time.deltaTime * truckMovementSpeed);
+        //Get user input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //Control truck forward and backward movement
+        transform.Translate(Vector3.forward * Time.deltaTime * truckMovementSpeed * verticalInput);
+
+        //Control truck side-to-side movement
+        transform.Rotate(Vector3.up * Time.deltaTime * truckTurnSpeed * horizontalInput);
     }
 }
