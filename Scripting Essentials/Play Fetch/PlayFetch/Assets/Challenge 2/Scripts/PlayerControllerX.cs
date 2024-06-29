b03c5c06503b86372d888f3c4725bf6fabe51753
Juliana Nielson <nielsonjuliana@gmail.com>
2024-06-28 22:29:20 -0600

    Challenge and both bonuses Completed

---
+++ Scripting Essentials/Play Fetch/PlayFetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float inputCooldown = 1.5f;
+    private bool inputAllowed = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && inputAllowed)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            StartCoroutine(WaitForCooldown());
+        }
+    }
+
+    IEnumerator WaitForCooldown()
+    {
+        //Only allow spawning dogs after cooldown
+        inputAllowed = false;
+        yield return new WaitForSeconds(inputCooldown);
+        inputAllowed = true;
+    }
+}
