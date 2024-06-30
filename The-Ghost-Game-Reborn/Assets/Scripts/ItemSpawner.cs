251f06b6c19843d754f28b262b7c017a09827e8b
Juliana Nielson <nielsonjuliana@gmail.com>
2024-06-29 19:49:03 -0600

    Global instance code
    All script managers should only have one version at a time

--- The-Ghost-Game-Reborn/Assets/Scripts/ItemSpawner.cs
+++ The-Ghost-Game-Reborn/Assets/Scripts/ItemSpawner.cs
@@ -6,26 +6,41 @@ public class ItemSpawner : MonoBehaviour
 {
     [SerializeField] private List<GameObject> objectiveItems;
     [SerializeField] private List<Vector3> spawnLocations;
+
+    public static ItemSpawner Instance { get; private set; }
+
+    private void Awake()
+    {
+        //Only ever have one version of this game object at a time
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
         spawnObjectiveItems();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void spawnObjectiveItems()
     {
+        //Make a list copy of all posible spawn locations
         List<Vector3> avaliableLocations = spawnLocations;
 
         for (int i = 0; i < objectiveItems.Count; i++)
         {
+            //Choose a random location
             int locationIndex = Random.Range(0, avaliableLocations.Count);
 
+            //Spawn item at random location
             Instantiate(objectiveItems[i], avaliableLocations[locationIndex], objectiveItems[i].transform.rotation);
+
+            //Remove this location from the list copy so it can't be used for more than one object
             avaliableLocations.RemoveAt(locationIndex);
         }
     }
