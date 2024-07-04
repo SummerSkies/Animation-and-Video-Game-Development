385f7c9f2091dd1ce31b247f29c09e4195b1b239
Juliana Nielson <nielsonjuliana@gmail.com>
2024-07-03 21:30:59 -0600

    Don't Just Stand There

--- Scripting Essentials/Sound and Effects/SoundAndEffects/Assets/Scripts/PlayerController.cs
+++ Scripting Essentials/Sound and Effects/SoundAndEffects/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,23 @@ public class PlayerController : MonoBehaviour
     public bool gameIsOver = false;
 
     private Rigidbody rb;
+    private Animator animator;
     private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !gameIsOver)
         {
+            animator.SetTrigger("Jump_trig");
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
@@ -38,6 +41,8 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameIsOver = true;
+            animator.SetBool("Death_b", true);
+            animator.SetInteger("DeathType_int", 1);
         }
     }
 }
