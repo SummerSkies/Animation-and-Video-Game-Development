0198e1328b60c4102d5f34cebc8c23c7638fda39
Juliana Nielson <nielsonjuliana@gmail.com>
2024-07-11 19:28:02 -0600

    Watch Where You're Going

---
+++ Scripting Essentials/SumoBattlePrototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 8.0f;
+    [SerializeField] private GameObject focalPoint;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(focalPoint.transform.forward * verticalInput * speed);
+    }
+}
