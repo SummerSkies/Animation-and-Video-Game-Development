48e615a85cf3a8d464c59854dd352e327dbdce80
Juliana Nielson <nielsonjuliana@gmail.com>
2024-07-20 20:14:28 -0600

    Created playable character prefabs

---
+++ The-Ghost-Game-Reborn/Assets/Scripts/CharacterLoader.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLoader : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> playableCharacters;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
