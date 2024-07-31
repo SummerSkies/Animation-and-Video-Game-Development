a071670bb970e32ec561ba706e2d91ffa6eb0a24
Juliana Nielson <nielsonjuliana@gmail.com>
2024-07-30 18:25:16 -0600

    Added Game Over Message

--- Scripting Essentials/UFODefenseForce/Assets/Scripts/DetectTriggers.cs
+++ Scripting Essentials/UFODefenseForce/Assets/Scripts/DetectTriggers.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DetectTriggers : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI gameOverText;
     private void Awake()
     {
         Time.timeScale = 1.0f;
@@ -11,5 +13,6 @@ public class DetectTriggers : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Time.timeScale = 0;
+        gameOverText.gameObject.SetActive(true);
     }
 }
