5b4253cd977a9e79e1690ef05493ba289536449b
Juliana Nielson <nielsonjuliana@gmail.com>
2024-08-02 13:20:03 -0600

    Pickup flashes between two colors

---
+++ Scripting Essentials/UFODefenseForce/Assets/Scripts/FlashColor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashColor : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    [SerializeField] private Color baseColor;
+    [SerializeField] private Color flashColor;
+    [SerializeField] private float flashSpeed;
+
+    private void Start()
+    {
+        StartCoroutine(FlashColors());
+    }
+
+    IEnumerator FlashColors()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(flashSpeed);
+            material.color = flashColor;
+            yield return new WaitForSeconds(flashSpeed);
+            material.color = baseColor;
+        }
+    }
+}
+
