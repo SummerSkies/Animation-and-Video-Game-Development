29de238dccfe97a36897017c47839a79fd010d28
Juliana Nielson <94935811+SummerSkies@users.noreply.github.com>
2024-11-18 20:02:10 -0700

    Added icons that fill when their respective item is collected.

--- The-Ghost-Game-Reborn/Assets/Scripts/ObjectiveItemBehaviour.cs
+++ The-Ghost-Game-Reborn/Assets/Scripts/ObjectiveItemBehaviour.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectiveItemBehaviour : MonoBehaviour
 {
-    
+    [SerializeField] private string iconName;
+
+    private GameObject icon;
+    private IconBehaviour iconBehaviour;
+
+    private void Start()
+    {
+        icon = GameObject.Find(iconName);
+        iconBehaviour = icon.GetComponent<IconBehaviour>();
+    }
+
+    private void OnDestroy()
+    {
+        if (icon != null)
+        {
+            iconBehaviour.fillIcon();
+        }
+    }
 }
